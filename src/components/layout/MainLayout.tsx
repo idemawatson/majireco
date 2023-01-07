f08c3680a18a4d33f7d305649b82cf04e1043e8d
井手拓海 <idetakumi@idetakuminoMacBook-Pro.local>
2023-01-07 18:53:29 +0900

    lint:不要なimportの削除

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -4,8 +4,6 @@ import { FC, ReactNode } from 'react'
 import { TheAppBackground } from '@/components/uiParts/TheAppBackground'
 import TheAppBar from '@/components/uiParts/TheAppBar'
 import { TheBottomNavigation } from '@/components/uiParts/TheBottomNavigation'
-import { styled } from '@mui/material'
-import { grey } from '@mui/material/colors'
 
 export const MainLayout: FC<{ children: ReactNode }> = ({ children }) => {
   const Offset = styled('div')(({ theme }) => ({
